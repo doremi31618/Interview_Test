c81808c4a8089d84499c57c6e063627c0a6039d3
EricZhan <doremi31618@gmail.com>
2019-07-03 15:56:09 +0800

    update

---
+++ Assets/Scripts/Test2/PathFinding/GameObjectManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//refence : https://www.storynest.com/pix/_5exp/e_2014_0417_roach/p0.php?lang=ch
+
+[System.Serializable]
+public struct CreatureData
+{
+    [Header("Creature Perception Attribbute")]
+    [Tooltip("the acurate of ray")][Range(3,100)]public int rayNumber;
+    [Range(0.01f,1)]public float rayWidth;
+    [Tooltip("the angle of eyes perception")][Range(90,270)]public float eyeSight;
+    [Tooltip("the distance of eyes perception")][Range(1,15)]public float viewDistance;
+    public Color rayColor;
+    public bool debugVisualizer;
+
+    [Header("Creature Motion Atrribte")]
+    public float speed;
+    public bool trailVisualizer;
+}
+public class GameObjectManager : MonoBehaviour
+{
+    
+    [Header("Creature Generate")]
+    public int nuberOfCreatures = 30;
+    public GameObject prefab;
+
+    public CreatureData all_CreatureData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Test2/PathFinding/RayPerception.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode][RequireComponent(typeof(LineRenderer))]
+public class RayPerception : MonoBehaviour
+{
+
+    public  GameObjectManager creatureObjectManager;
+    public LineRenderer m_lineRender;
+    public LayerMask detectLayer;
+    //[HideInInspector]
+    public List<detectObject> detectList = new List<detectObject>();
+
+    
+    [System.Serializable]public struct detectObject
+    {
+        public Transform detectObjectTransform;
+        public float distanceToDetectPoint;
+        public Vector3 hitPoint;
+        public Vector3 obstacleDirection;
+
+        public Vector3 obstacleVelocity;
+    }
+
+
+#region private attribute
+    float adjustmentAngle;
+   
+#endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayGenorator();
+    }
+
+    void Initialize()
+    {
+        //Initialize parent GameObject
+        if(creatureObjectManager == null)creatureObjectManager = transform.parent.gameObject.GetComponent< GameObjectManager>();
+
+        //Initialize attribute;
+        
+    }
+
+    void RayGenorator()
+    {
+        if(creatureObjectManager == null)creatureObjectManager = transform.parent.gameObject.GetComponent< GameObjectManager>();
+        if(!creatureObjectManager.all_CreatureData.debugVisualizer)
+        {
+            m_lineRender.enabled = false;
+            return;
+        }
+        else if(creatureObjectManager.all_CreatureData.debugVisualizer && !m_lineRender.enabled)
+        {
+            m_lineRender.enabled = true;
+        }
+        float adjustmentAngle = (180 - creatureObjectManager.all_CreatureData.eyeSight) * 0.5f - transform.rotation.eulerAngles.y;
+
+        ///setting up line render
+        if(creatureObjectManager.all_CreatureData.debugVisualizer)
+        {
+            m_lineRender.positionCount = (creatureObjectManager.all_CreatureData.rayNumber ) * 2;
+
+            //setting width
+            m_lineRender.startWidth = creatureObjectManager.all_CreatureData.rayWidth;
+            m_lineRender.endWidth = creatureObjectManager.all_CreatureData.rayWidth;
+
+            //setting color
+            m_lineRender.startColor = creatureObjectManager.all_CreatureData.rayColor;
+            m_lineRender.endColor = creatureObjectManager.all_CreatureData.rayColor;
+        }
+        
+        detectList.Clear();
+
+        //render laser
+        for(int i = 0 ; i<creatureObjectManager.all_CreatureData.rayNumber;i++)
+        {
+
+            float a = creatureObjectManager.all_CreatureData.eyeSight * i/(creatureObjectManager.all_CreatureData.rayNumber-1) + adjustmentAngle ;
+            
+            float x = creatureObjectManager.all_CreatureData.viewDistance * Mathf.Cos(Mathf.Deg2Rad * a) ;
+            float z = creatureObjectManager.all_CreatureData.viewDistance * Mathf.Sin(Mathf.Deg2Rad * a) ;
+            float y = 0;
+            Vector3 dir = new Vector3(x,y,z);
+
+            Ray ray = new Ray(transform.position,dir);
+            RaycastHit hit;
+
+            detectObject _detectObject = new detectObject();
+
+            //detect if hit something
+            if(Physics.Raycast(ray,out hit, creatureObjectManager.all_CreatureData.viewDistance, detectLayer))
+            {
+
+                if(creatureObjectManager.all_CreatureData.debugVisualizer)
+                {
+                    m_lineRender.SetPosition(i*2, transform.position);
+                    m_lineRender.SetPosition(i*2+1, hit.point);
+                }
+
+                _detectObject.detectObjectTransform = hit.transform;
+                _detectObject.distanceToDetectPoint = (hit.point - transform.position).magnitude;
+                _detectObject.hitPoint = hit.point;
+                _detectObject.obstacleDirection = hit.point - transform.position;
+                _detectObject.obstacleVelocity = hit.transform.GetComponent<Rigidbody>().velocity;
+
+                detectList.Add(_detectObject);
+
+                //Debug.DrawRay(transform.position,hit.point - transform.position,creatureObjectManager.all_CreatureData.rayColor,creatureObjectManager.all_CreatureData.viewDistance);
+            }
+            else
+            {
+                if(creatureObjectManager.all_CreatureData.debugVisualizer)
+                {
+                    m_lineRender.SetPosition(i*2, transform.position);
+                    m_lineRender.SetPosition(i*2+1, dir + transform.position);
+                }
+                //Debug.DrawRay(transform.position,dir,creatureObjectManager.all_CreatureData.rayColor,creatureObjectManager.all_CreatureData.viewDistance);
+            }
+            
+            
+        }
+    }
+
+
+
+}
